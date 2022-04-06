eb8b24d4a36f0ce8047381f7d0b9bf90eaa88591
Burak Kılıç <burakilic43@gmail.com>
2022-04-07 02:00:25 +0300

    AOP
    AOP

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,9 +9,12 @@ namespace Business.Constants
 {
     public static class Messages
     {
+        public static string ProductCountOfCategoryError = "En fazla 10 ürün eklenebilir";
         public static string ProductAdded = "Ürün Eklendi";
         public static string ProductNameInValid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda ";
         public static string ProductListed = " Ürünler Listelendi";
+        public  static string ProdcutNameAlreadyExists ="Bu isimde bir ürün mevcut";
+        public  static string CategoryLimitExceded= "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
